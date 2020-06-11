745399369b2561a28ca1bae670cba1f49083b456
Jessica Clarke <jrtc27@jrtc27.com>
2020-06-11 05:03:13 +0000

    Add high-level convenience APIs around front end
    This factors out all the necessary logic for spawning threads, dealing
    with sockets and stopping the threads later. Applications should not
    need to call the front end directly unless they have very specific needs
    (and any new transports should be added as additional wrappers).
    
    Consumers just need to implement dmi_read/dmi_write and call
    gdbstub_start_*/gdbstub_stop/gdbstub_join as appropriate.

---
+++ src/gdbstub.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 Bluespec, Inc. All Rights Reserved
+// Author: Rishiyur Nikhil
+//
+// ================================================================
+
+#pragma once
+
+// ****************************************************************
+// ****************************************************************
+// HIGH LEVEL API (called from the platform-specific top-level code)
+// ****************************************************************
+// ****************************************************************
+
+// ================================================================
+// Spawn a new thread for main_gdbstub with a pipe set up for later
+// stopping it.
+
+extern
+void gdbstub_start_fd (FILE *logfile, int gdb_fd);
+
+// ================================================================
+// Spawn a new thread listening for sockets which are then used to
+// call main_gdbstub, with a pipe set up for later stopping it.
+// Returns the port bound to (useful if the provided port is 0), or
+// -1 on error
+
+extern
+int gdbstub_start_tcp (FILE *logfile, unsigned short port);
+
+// ================================================================
+// Stop the gdbstub thread.
+
+extern
+void gdbstub_stop (void);
+
+// ================================================================
+// Wait for the gdbstub thread to exit.
+
+extern
+void gdbstub_join (void);
+
+// ================================================================
